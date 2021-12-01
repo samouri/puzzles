926651df580df39a425628ae0906d9f48483ae3b
Jake F - @samouri <samouri@users.noreply.github.com>
2021-12-01 15:17:05 -0500

    part 2

--- advent of code/2021/src/main.rs
+++ advent of code/2021/src/main.rs
@@ -1,18 +1,35 @@
 use std::fs;
 
 fn main() -> std::io::Result<()> {
+  one()
+}
+
+fn one() -> std::io::Result<()> {
   let path = "src/01/input.txt";
   let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
   let lines = contents.lines();
   let nums: Vec<i32> = lines.map(|n| n.parse::<i32>().unwrap()).collect();
 
+  // part 1
   let mut count = 0;
   for i in 1..nums.len() {
     if nums[i] > nums[i - 1] {
       count += 1
     }
   }
+  println!("Part 1: {}\n", count);
 
-  println!("Answer: {}\n", count);
+  // part 2
+  count = 0;
+  for i in 3..nums.len() {
+    let prev = nums[i - 3] + nums[i - 2] + nums[i - 1];
+    let curr = nums[i - 2] + nums[i - 1] + nums[i];
+
+    if curr > prev {
+      count += 1
+    }
+  }
+  println!("Part 2: {}\n", count);
+
   Ok(())
 }
