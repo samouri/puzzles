9b91045c776ed3416ef49334410c51349db0e044
Jake Fried <jakefried@Jakes-MacBook-Pro-2.local>
2021-12-02 21:06:35 -0500

    day2

--- advent of code/2021/src/main.rs
+++ advent of code/2021/src/main.rs
@@ -1,7 +1,11 @@
 use std::fs;
 
 fn main() -> std::io::Result<()> {
-  one()
+  println!("Problem 1\n-----------");
+  one().unwrap();
+
+  println!("Problem 2\n-----------");
+  two()
 }
 
 fn one() -> std::io::Result<()> {
@@ -17,7 +21,7 @@ fn one() -> std::io::Result<()> {
       count += 1
     }
   }
-  println!("Part 1: {}\n", count);
+  println!("Part 1: {}", count);
 
   // part 2
   count = 0;
@@ -30,6 +34,48 @@ fn one() -> std::io::Result<()> {
     }
   }
   println!("Part 2: {}\n", count);
+
+  Ok(())
+}
+
+fn two() -> std::io::Result<()> {
+  let path = "src/02/input.txt";
+  let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+  let instructions: Vec<(&str, i32)> = contents
+    .lines()
+    .map(|l| l.split_at(l.find(" ").unwrap()))
+    .map(|(dir, count)| (dir.trim(), count.trim().parse::<i32>().unwrap()))
+    .collect();
+
+  // Part 1
+  let mut depth = 0;
+  let mut horizontal = 0;
+  for (dir, count) in &instructions {
+    if dir == &"down" {
+      depth += count
+    } else if dir == &"up" {
+      depth -= count
+    } else if dir == &"forward" {
+      horizontal += count
+    }
+  }
+  println!("Part 1: {}", depth * horizontal);
+
+  // Part 2
+  depth = 0;
+  horizontal = 0;
+  let mut aim = 0;
+  for (dir, count) in &instructions {
+    if dir == &"down" {
+      aim += count
+    } else if dir == &"up" {
+      aim -= count
+    } else if dir == &"forward" {
+      horizontal += count;
+      depth += aim * count;
+    }
+  }
+  println!("Part 2: {}", depth * horizontal);
 
   Ok(())
 }
