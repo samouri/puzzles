caee4f7ec969f80540993807c995902e3a2cae59
Jake <4656974+samouri@users.noreply.github.com>
2021-12-14 20:39:25 -0500

    zohdi refactor help

--- advent of code/2021/src/day12/mod.rs
+++ advent of code/2021/src/day12/mod.rs
@@ -1,89 +1,57 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-    iter::FromIterator,
-};
+use std::{collections::HashMap, fs};
 
 pub fn solve() -> std::io::Result<()> {
-    let path = "src/day12/emma.txt";
+    let path = "src/day12/example.txt";
     let file_content: String = fs::read_to_string(path).unwrap();
-    let mut caves: HashMap<String, Vec<String>> = HashMap::new();
+    let mut caves: HashMap<&str, Vec<&str>> = HashMap::new();
 
     for line in file_content.lines() {
         let splits: Vec<&str> = line.split("-").collect();
         let node1 = splits[0];
         let node2 = splits[1];
         if !caves.contains_key(node1) {
-            caves.insert(node1.to_owned(), Vec::new());
+            caves.insert(node1, Vec::new());
         }
         if !caves.contains_key(node2) {
-            caves.insert(node2.to_owned(), Vec::new());
+            caves.insert(node2, Vec::new());
         }
-        caves.get_mut(node1).unwrap().push(node2.to_owned());
-        caves.get_mut(node2).unwrap().push(node1.to_owned());
+        caves.get_mut(node1).unwrap().push(node2);
+        caves.get_mut(node2).unwrap().push(node1);
     }
 
-    let stack = vec!["start".to_owned()];
-    let paths = dfs(&caves, &stack, false);
+    let paths = dfs(&caves, vec![], "start", false);
     println!("Part 1: {}", paths.len());
 
-    let paths = dfs(&caves, &stack, true);
+    let paths = dfs(&caves, vec![], "start", true);
     println!("Part 2: {}", paths.len());
 
     Ok(())
 }
 
-fn dfs(
-    graph: &HashMap<String, Vec<String>>,
-    visited: &Vec<String>,
+fn dfs<'a>(
+    graph: &'a HashMap<&str, Vec<&str>>,
+    mut visited: Vec<&'a str>,
+    curr: &'a str,
     visit_twice: bool,
-) -> Vec<Vec<String>> {
-    let curr: String = (*visited.last().unwrap()).to_owned();
-    let naybs = graph.get(&curr).unwrap();
-    let mut paths: Vec<Vec<String>> = Vec::new();
+) -> Vec<Vec<&'a str>> {
+    visited.push(curr);
+    if curr == "end" {
+        return vec![visited.clone()];
+    }
 
-    for nayb in naybs {
-        let is_small = nayb.to_lowercase() == *nayb;
-        let mut visit_twice = visit_twice;
+    let naybs = graph.get(&curr).unwrap();
+    let mut paths: Vec<Vec<&str>> = Vec::new();
+    for &nayb in naybs {
         if nayb == "start" {
             continue;
-        } else if is_small && visited.contains(nayb) {
-            if !visit_twice {
-                continue;
-            }
-            visit_twice = false;
         }
-
-        let mut copy = copy_vec(&visited);
-        copy.push(nayb.clone());
-        if nayb == "end" {
-            paths.push(copy);
-        } else {
-            let subpaths = dfs(graph, &copy, visit_twice);
-            for subpath in subpaths {
-                paths.push(subpath);
-            }
+        let seen = visited.contains(&nayb);
+        let is_big = nayb.chars().all(char::is_uppercase);
+        if is_big || !seen || visit_twice {
+            let visit_twice = visit_twice && (is_big || !seen);
+            paths.append(&mut dfs(graph, visited.clone(), nayb, visit_twice));
         }
     }
 
     return paths;
-}
-
-fn copy_vec(str_vec: &Vec<String>) -> Vec<String> {
-    let mut v: Vec<String> = Vec::new();
-    for str in str_vec {
-        v.push(str.clone())
-    }
-    v
-}
-
-fn _dedupe_vec(vecs: &Vec<Vec<String>>) -> Vec<Vec<String>> {
-    let mut strs: Vec<String> = vecs.iter().map(|v: &Vec<String>| v.join(",")).collect();
-    strs.sort();
-    let deduped: HashSet<&str> = HashSet::from_iter(strs.iter().map(|s| s.as_str()));
-    println!("{}", strs.join("\n"));
-    deduped
-        .into_iter()
-        .map(|s| s.split(",").map(|s| s.to_owned()).collect())
-        .collect()
 }
